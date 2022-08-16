2a1b5e5f6900ae8386b46a7a7ce3a1f08c345192
Gbolahan <yustee2017@gmai.com>
2022-08-16 15:06:11 +0100

    init

--- src/Practice/Portfolio/Portfolio.jsx
+++ src/Practice/Portfolio/Portfolio.jsx
@@ -1,10 +1,69 @@
 import React from 'react'
 import './portfolio.css'
 
+import  { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination"
+
+// import required modules
+import { EffectCoverflow, Pagination } from "swiper";
+import data from './data';
+
+// import 'swiper/swiper.min.css'
+
+
 export default function Portfolio() {
   return (
     <div className='portfolio' id='portfolio'>
-        <h3>Portfolio Page in progress</h3>
+      <div className="top">
+        <h1>Portfolio</h1>
+        <p>Most recent work</p>
+      </div>
+         <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          // loop={true}
+          // loopedSlides={9}
+          centeredSlides={true}
+          slidesPerView={window.innerWidth > 800  ?  3 : "auto"}
+          // slidesPerView={2}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+            }}
+            pagination={{
+              clickable: true
+            }}
+            modules={[EffectCoverflow, Pagination]}
+            className="mySwiper"
+            keyboard={true}
+            mousewheel={true}
+            scrollbar={{ draggable: true }}
+            
+            
+          >
+        {data.map((item, index) => {
+          return(<SwiperSlide key={index}>
+            <div className="photo">
+              <img src={item.image} alt={item.name} />
+            </div>
+            <div className="desc">
+              <h3>{item.name}</h3>
+              <p>{item.desc}</p>
+              <a href={item.link} className="btn explore">Explore</a>
+            </div>
+          </SwiperSlide>)
+        })}
+      </Swiper>
+    
     </div>
   )
 }
---
+++ src/Practice/Portfolio/data.jsx
@@ -0,0 +1,82 @@
+const data = [
+    {
+        id: 1,
+        name: "Modal",
+        link : "Sport",
+        image: "../../Images/Modal.png",
+        desc: "Modal Popup | UI element that appears in the foreground of a website",
+        price: "$1,200.00"
+    },
+    {
+        id: 2,
+        name: "Accordion",
+        link : "Adventure",
+        image: "../../Images/Accordion.png",
+        desc: "Useful when when you want to toggle between hiding and showing large amount of content."
+    },
+    {
+        id: 3,
+        name: "Four Cards",
+        link : "Sport",
+        image: "../../Images/Four-cards.png",
+        desc: "Frontend Mentor Challenge | Four card feature section"
+    },
+    {
+        id: 4,
+        name: "Learning Website",
+        link : "Sport",
+        image: "../../Images/Learning-web.png",
+        desc:"French Learning website created using CSS Grid."
+    },
+    {
+        id: 5,
+        name: "Component Signup",
+        link : "Racing",
+        image: "../../Images/Component-signup.png",
+        desc: "Frontend Mentor Challenge | Intro component with sign up form"
+    },
+    {
+        id: 6,
+        name: "Redmi Note 9s Control Panel",
+        link : "Action",
+        image: "../../Images/Redmi.png",
+        desc: "Redmi Note 9s control panel craeted with HTML, CSS and JAVASCRIPT."
+    },
+    {
+        id: 7,
+        name: "Sign up form + Validation",
+        link : "Action",
+        image: "../../Images/Form.png",
+        desc: "User registration with Form validation using Javascript"
+    },
+    {
+        id: 8,
+        name: "Counter App",
+        link : "Racing",
+        image: "../../Images/Counter-app.png",
+        desc: "Javascript Counter Application."
+    },
+    {
+        id: 9,
+        name: "Profile Card",
+        link : "Action",
+        image: "../../Images/Profile-card.png",
+        desc: "Frontend Mentor Challenge | Profile card component"
+    },
+    {
+        id: 10,
+        name: "U-secured web",
+        link : "Action",
+        image: "../../Images/U-secured.png",
+        desc: "Safety tech web application."
+    },
+    {
+        id: 11,
+        name: "Blog web",
+        link : "Action",
+        image: "../../Images/U-secured.png",
+        desc: "Fashion and Lifestyle blog created with HTML , CSS and Javascript"
+    }
+]
+
+export default data
